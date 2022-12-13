0851920f87283e62021d969a7672b8ca0112e9e4
= <=>
2022-12-13 17:23:34 +0700

    lab7_3_complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
+
+int adiff(int adeg,int bdeg){
+  int sumdeg=abs(adeg-bdeg) %360;
+  if (sumdeg>180){
+    sumdeg=360-sumdeg ;
+  }
+
+
+
+  return sumdeg ;
+}
 
 
 int main(){
+
   cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  return 0;
 }
